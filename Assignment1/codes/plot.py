0214652ed2005cafce35bc6b5d78f63e9926cf7c
Prajwal M <ee23btech11217@iith.ac.in>
2024-01-08 00:03:00 +0530

    u(n)

--- Assignment1/codes/plot.py
+++ Assignment1/codes/plot.py
@@ -2,7 +2,7 @@ import math
 import matplotlib.pyplot as plt
 
 def x_n(n):
-    if n <= 0:
+    if n < 0:
         return 0
     else:
         return (-1) / math.factorial(n)
@@ -12,7 +12,7 @@ print("//This program will plot the terms of the sequence up to x(n)// \n")
 N = int(input("Enter the value of n: "))
 
 # Generate the sequence up to N
-n_values = list(range(N+1))
+n_values = list(range(-2, N+1))
 x_values = [x_n(n) for n in n_values]
 
 # Plot the terms
