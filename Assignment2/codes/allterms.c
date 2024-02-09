39b7a0dde52d21a7ebb3f3a6c7f5b806ba9daf3f
Prajwal M <ee23btech11217@iith.ac.in>
2024-02-09 10:29:02 +0530

    create values.dat

--- Assignment2/codes/allterms.c
+++ Assignment2/codes/allterms.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int main() {
-    int n, N;
-    printf("\\This program will print the values of the terms of the sequence upto x(n)\\\n");
-    printf("Enter value of n: ");
-    scanf("%d", &N);
+    FILE *fp;
+    fp = fopen("values.dat", "w"); // Open for writing in text mode
 
-    // Printing the series header
-    printf("... \nx(0) = 0\n");
-
-    // Calculate and print the terms of the series for n = 0 to 10 (you can change the range as needed)
-    for (n = 1; n <= N; ++n) {
-        // Print the result in fraction format
-        printf("x(%d) = %lld/4\n", n, (n+2)*(n+2));
+    if (fp == NULL) {
+        printf("Error opening file!\n");
+        return 1;
     }
+
+    for (int n = 0; n <= 10; n++) {
+        double xn = (n + 2)*(n + 2) / 4.0;
+        fprintf(fp, "%d %lf\n", n, xn); // Write n and xn to file, separated by a space
+    }
+
+    fclose(fp); // Close the file
 
     return 0;
 }
--- Assignment2/codes/allterms.py
+++ Assignment2/codes/allterms.py
@@ -1,35 +1,13 @@
-import math
+import numpy as np
 import matplotlib.pyplot as plt
 
-# Defining x(n)
-def x_n(n):
-    if n < 0:
-        return 0
-    else:
-        return (n+2)*(n+2)/4.0
+# Load the data from 'values.dat'
+data = np.loadtxt('values.dat')
+n = data[:, 0].astype(int)  # n values as integers
+xn = data[:, 1]  # x(n) values as floats
 
-# Get N from the user
-print("//This program will plot the terms of the sequence up to x(n)// \n")
-N = int(input("Enter the value of n: "))
-
-# Generate the sequence up to N
-print("...\nx( -1 ) = 0\n")
-for x in range(N+1):
-    {    
-        print("x(", x, ") = ", x_n(x), "\n")
-    }
-print("...\n")
-
-# Generate the sequence up to N
-n_values = list(range(-2, N+1))
-x_values = [x_n(n) for n in n_values]
-
-# Plot the terms
-plt.stem(n_values, x_values, markerfmt='o', linefmt='b-', basefmt='r-')
-plt.title('Terms of the Sequence x(n)')
+# Plotting
+plt.stem(n, xn, basefmt=" ")
 plt.xlabel('n')
-plt.ylabel('x(n)')
-plt.grid(True)
-plt.show()
-plt.savefig("allterms.png")
-
+plt.ylabel('$x(n)$')
+plt.savefig('plot.png', dpi = 300, bbox_inches = 'tight')
